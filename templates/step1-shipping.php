84b1164a3bf59d3455d334c287f3a02698dc810b
Niko.Knez <info@design-nk.de>
2023-08-21 22:27:03 +0200

    update

--- templates/step1-shipping.php
+++ templates/step1-shipping.php
@@ -1,43 +1,55 @@
-<div class="checkout-step checkout-step1">
-    <h2>Versand</h2>
-    <form id="shipping-form">
-        <div class="form-group">
-            <label for="first-name">Vorname</label>
-            <input type="text" id="first-name" name="first-name" required>
-        </div>
-        <div class="form-group">
-            <label for="last-name">Nachname</label>
-            <input type="text" id="last-name" name="last-name" required>
-        </div>
-        <div class="form-group">
-            <label for="street">Straße und Hausnummer</label>
-            <input type="text" id="street" name="street" required>
-        </div>
-        <div class="form-group">
-            <label for="postcode">Postleitzahl</label>
-            <input type="text" id="postcode" name="postcode" required>
-        </div>
-        <div class="form-group">
-            <label for="city">Stadt</label>
-            <input type="text" id="city" name="city" required>
-        </div>
-        <div class="form-group">
-            <label for="country">Land</label>
-            <select id="country" name="country" required>
-                <option value="de">Deutschland</option>
-                <option value="at">Österreich</option>
-                <option value="ch">Schweiz</option>
-                <!-- Add more countries as needed -->
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="phone">Telefonnummer</label>
-            <input type="tel" id="phone" name="phone" required>
-        </div>
-        <div class="form-group">
-            <label for="email">E-Mail-Adresse</label>
-            <input type="email" id="email" name="email" required>
-        </div>
-        <button type="submit" class="btn-next">Weiter</button>
+<div class="step1-shipping">
+    <h2>Versanddetails</h2>
+    <form id="step1-form">
+        <label for="first-name">Vorname:</label>
+        <input type="text" id="first-name" name="first-name" required>
+        <label for="last-name">Nachname:</label>
+        <input type="text" id="last-name" name="last-name" required>
+        <label for="address">Adresse:</label>
+        <input type="text" id="address" name="address" required>
+        <label for="city">Stadt:</label>
+        <input type="text" id="city" name="city" required>
+        <label for="postcode">Postleitzahl:</label>
+        <input type="text" id="postcode" name="postcode" required>
+        <label for="country">Land:</label>
+        <select id="country" name="country">
+            <option value="de">Deutschland</option>
+            <option value="at">Österreich</option>
+            <option value="ch">Schweiz</option>
+        </select>
+        <input type="hidden" id="current-step" value="1">
+        <button id="next-step">Weiter</button>
     </form>
 </div>
+
+<script>
+    jQuery(document).ready(function($) {
+        $('#next-step').on('click', function(e) {
+            e.preventDefault();
+            var currentStep = parseInt($('#current-step').val());
+            var nextStep = currentStep + 1;
+            var url = window.location.href;
+            var newUrl = updateUrlParameter(url, 'step', nextStep);
+            window.location.href = newUrl;
+        });
+
+        function updateUrlParameter(url, param, paramVal) {
+            var newAdditionalURL = "";
+            var tempArray = url.split("?");
+            var baseURL = tempArray[0];
+            var additionalURL = tempArray[1];
+            var temp = "";
+            if (additionalURL) {
+                tempArray = additionalURL.split("&");
+                for (var i = 0; i < tempArray.length; i++) {
+                    if (tempArray[i].split('=')[0] != param) {
+                        newAdditionalURL += temp + tempArray[i];
+                        temp = "&";
+                    }
+                }
+            }
+            var rows_txt = temp + "" + param + "=" + paramVal;
+            return baseURL + "?" + newAdditionalURL + rows_txt;
+        }
+    });
+</script>
--- templates/step2-payment.php
+++ templates/step2-payment.php
@@ -19,5 +19,39 @@
             <input type="text" name="coupon_code" id="coupon_code" placeholder="Gutscheincode eingeben">
             <button type="submit" name="apply_coupon" value="Gutschein anwenden">Gutschein anwenden</button>
         </div>
+        <input type="hidden" id="current-step" value="2">
+        <button id="next-step">Weiter</button>
     </form>
 </div>
+
+<script>
+    jQuery(document).ready(function($) {
+        $('#next-step').on('click', function(e) {
+            e.preventDefault();
+            var currentStep = parseInt($('#current-step').val());
+            var nextStep = currentStep + 1;
+            var url = window.location.href;
+            var newUrl = updateUrlParameter(url, 'step', nextStep);
+            window.location.href = newUrl;
+        });
+
+        function updateUrlParameter(url, param, paramVal) {
+            var newAdditionalURL = "";
+            var tempArray = url.split("?");
+            var baseURL = tempArray[0];
+            var additionalURL = tempArray[1];
+            var temp = "";
+            if (additionalURL) {
+                tempArray = additionalURL.split("&");
+                for (var i = 0; i < tempArray.length; i++) {
+                    if (tempArray[i].split('=')[0] != param) {
+                        newAdditionalURL += temp + tempArray[i];
+                        temp = "&";
+                    }
+                }
+            }
+            var rows_txt = temp + "" + param + "=" + paramVal;
+            return baseURL + "?" + newAdditionalURL + rows_txt;
+        }
+    });
+</script>
