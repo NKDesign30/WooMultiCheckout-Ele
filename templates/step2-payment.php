5afc106c914cec4bcce3650a3afaee4442abb336
NkDesign <70699589+NKDesign30@users.noreply.github.com>
2023-08-21 16:46:56 +0200

    Update step2-payment.php

--- templates/step2-payment.php
+++ templates/step2-payment.php
@@ -2,11 +2,16 @@
     <h2><?php _e('Schritt 2: Zahlung', 'woomulticheckout'); ?></h2>
     <form id="wmc-payment-form">
         <div class="wmc-field">
-            <label><?php _e('Zahlungsmethode', 'woomulticheckout'); ?></label>
-            <select id="wmc-payment-method" name="payment_method" required>
-                <!-- Add options for payment methods here -->
-            </select>
-        </div>
+    <label><?php _e('Zahlungsmethode', 'woomulticheckout'); ?></label>
+    <select id="wmc-payment-method" name="payment_method" required>
+        <?php
+        $payment_methods = wmc_get_payment_methods();
+        foreach ($payment_methods as $method) {
+            echo '<option value="' . esc_attr($method['id']) . '">' . esc_html($method['title']) . '</option>';
+        }
+        ?>
+    </select>
+</div>
         <div class="wmc-field">
             <label for="wmc-coupon-code"><?php _e('Gutscheincode', 'woomulticheckout'); ?></label>
             <input type="text" id="wmc-coupon-code" name="coupon_code">
