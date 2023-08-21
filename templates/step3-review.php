3c0749866291653819e0086f91fb575d6debba2d
NkDesign <70699589+NKDesign30@users.noreply.github.com>
2023-08-21 16:40:46 +0200

    Update step3-review.php

---
+++ templates/step3-review.php
@@ -0,0 +1,24 @@
+<div class="wmc-step wmc-step3">
+    <h2><?php _e('Schritt 3: Überprüfung', 'woomulticheckout'); ?></h2>
+    <div class="wmc-review-section">
+        <h3><?php _e('Zahlungsmethode', 'woomulticheckout'); ?></h3>
+        <p id="wmc-review-payment-method"><!-- Display selected payment method here --></p>
+    </div>
+    <div class="wmc-review-section">
+        <h3><?php _e('Rabattcode', 'woomulticheckout'); ?></h3>
+        <p id="wmc-review-coupon-code"><!-- Display applied coupon code here --></p>
+    </div>
+    <div class="wmc-review-section">
+        <h3><?php _e('Lieferadresse', 'woomulticheckout'); ?></h3>
+        <p id="wmc-review-shipping-address"><!-- Display shipping address here --></p>
+    </div>
+    <div class="wmc-review-section">
+        <h3><?php _e('Rechnungsadresse', 'woomulticheckout'); ?></h3>
+        <p id="wmc-review-billing-address"><!-- Display billing address here --></p>
+    </div>
+    <div class="wmc-review-section">
+        <h3><?php _e('Bestellung', 'woomulticheckout'); ?></h3>
+        <div id="wmc-review-order"><!-- Display order details here --></div>
+    </div>
+    <button type="button" id="wmc-place-order"><?php _e('Jetzt bestellen', 'woomulticheckout'); ?></button>
+</div>
