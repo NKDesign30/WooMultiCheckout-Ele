f4b54d1717d7932013a15d3e07669771fa852a02
NkDesign <70699589+NKDesign30@users.noreply.github.com>
2023-08-21 13:39:09 +0200

    Update step2-payment.php

---
+++ templates/step2-payment.php
@@ -0,0 +1,14 @@
+<div class="multistep-checkout-step2">
+    <h2>Zahlung</h2>
+    <form id="step2-form">
+        <label for="payment-method">Zahlungsmethode:</label>
+        <select id="payment-method" name="payment-method">
+            <!-- Add payment methods as needed -->
+            <option value="paypal">PayPal</option>
+            <option value="credit-card">Kreditkarte</option>
+        </select>
+        <label for="coupon-code">Gutscheincode:</label>
+        <input type="text" id="coupon-code" name="coupon-code">
+        <input type="submit" value="Weiter zu Schritt 3">
+    </form>
+</div>
