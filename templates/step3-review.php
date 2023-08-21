c0115e4e8881ae756b339b32ca49c121a281b008
NkDesign <70699589+NKDesign30@users.noreply.github.com>
2023-08-21 13:40:01 +0200

    Update step3-review.php

---
+++ templates/step3-review.php
@@ -0,0 +1,29 @@
+<div class="multistep-checkout-step3">
+    <h2>Überprüfung</h2>
+    <div class="review-section">
+        <h3>Zahlungsmethode:</h3>
+        <p id="review-payment-method"></p>
+        <h3>Rabattcode:</h3>
+        <p id="review-coupon-code"></p>
+        <h3>Lieferadresse:</h3>
+        <p id="review-shipping-address"></p>
+        <h3>Rechnungsadresse:</h3>
+        <p id="review-billing-address"></p>
+        <h3>Bestellung:</h3>
+        <table id="review-order-table">
+            <!-- Add order details as needed -->
+        </table>
+        <h3>Zwischensumme:</h3>
+        <p id="review-subtotal"></p>
+        <h3>Versandkosten:</h3>
+        <p id="review-shipping-costs"></p>
+        <h3>MwSt.:</h3>
+        <p id="review-tax"></p>
+        <h3>Rabatt:</h3>
+        <p id="review-discount"></p>
+        <h3>Gesamtsumme:</h3>
+        <p id="review-total"></p>
+    </div>
+    <button id="edit-order">Bestellung bearbeiten</button>
+    <button id="place-order">Jetzt bestellen</button>
+</div>
