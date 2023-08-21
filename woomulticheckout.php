f17352bddfa54b58304fb31769e14cadbfa225ef
Niko.Knez <info@design-nk.de>
2023-08-21 17:18:58 +0200

    route anpassen

--- woomulticheckout.php
+++ woomulticheckout.php
@@ -17,7 +17,7 @@ define('WMC_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('WMC_PLUGIN_URL', plugin_dir_url(__FILE__));
 
 // Include the widget class and functions
-require_once WMC_PLUGIN_PATH . 'includes/class-woomulticheckout.php';
+require_once plugin_dir_path(__FILE__) . 'class-woomulticheckout.php';
 require_once WMC_PLUGIN_PATH . 'includes/functions.php';
 
 // Register the widget in Elementor
