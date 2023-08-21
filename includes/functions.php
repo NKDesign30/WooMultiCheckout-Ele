5a58048b47f3ce6248fe7cce9f020e3612c7f476
NkDesign <70699589+NKDesign30@users.noreply.github.com>
2023-08-21 16:43:35 +0200

    Update functions.php

---
+++ includes/functions.php
@@ -0,0 +1,36 @@
+<?php
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+function wmc_apply_coupon_code($coupon_code) {
+    // Use WooCommerce functions to apply the coupon code
+    WC()->cart->add_discount(sanitize_text_field($coupon_code));
+}
+
+function wmc_place_order($shipping_info, $payment_info) {
+    // Create a new order in WooCommerce
+    $order = wc_create_order();
+
+    // Set the shipping and billing information
+    $order->set_address($shipping_info, 'shipping');
+    $order->set_address($payment_info, 'billing');
+
+    // Add the products from the cart to the order
+    foreach (WC()->cart->get_cart() as $cart_item) {
+        $order->add_product($cart_item['data'], $cart_item['quantity']);
+    }
+
+    // Set the payment method
+    $order->set_payment_method($payment_info['payment_method']);
+
+    // Calculate the totals and save the order
+    $order->calculate_totals();
+    $order->save();
+
+    // Empty the cart
+    WC()->cart->empty_cart();
+
+    return $order->get_id();
+}
