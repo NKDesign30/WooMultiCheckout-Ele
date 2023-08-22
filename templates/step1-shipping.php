5bf21e3079073d948c50c76af1dacfc4b6aff47b
Niko.Knez <info@design-nk.de>
2023-08-22 11:11:34 +0200

    update

--- templates/step1-shipping.php
+++ templates/step1-shipping.php
@@ -23,17 +23,48 @@
 </div>
 
 <script>
-    document.getElementById('next-step').addEventListener('click', function() {
-        var firstName = document.getElementById('first-name').value;
-        var lastName = document.getElementById('last-name').value;
-        var address = document.getElementById('address').value;
-        var city = document.getElementById('city').value;
-        var postcode = document.getElementById('postcode').value;
-        var country = document.getElementById('country').value;
-        var shippingAddress = firstName + ' ' + lastName + ', ' + address + ', ' + city + ', ' + postcode + ', ' + country;
-        localStorage.setItem('shippingAddress', shippingAddress);
+    jQuery(document).ready(function($) {
+        $('#next-step').on('click', function(e) {
+            e.preventDefault();
 
-        var billingAddress = shippingAddress; // Assuming billing address is the same as shipping address
-        localStorage.setItem('billingAddress', billingAddress);
+            // Hier können Sie den Code aus Ihrem JavaScript mit document.getElementById hinzufügen
+            console.log('Button clicked'); // Debugging-Anweisung
+            var firstName = document.getElementById('first-name').value;
+            var lastName = document.getElementById('last-name').value;
+            var address = document.getElementById('address').value;
+            var city = document.getElementById('city').value;
+            var postcode = document.getElementById('postcode').value;
+            var country = document.getElementById('country').value;
+            var shippingAddress = firstName + ' ' + lastName + ', ' + address + ', ' + city + ', ' + postcode + ', ' + country;
+            localStorage.setItem('shippingAddress', shippingAddress);
+
+            var billingAddress = shippingAddress; // Assuming billing address is the same as shipping address
+            localStorage.setItem('billingAddress', billingAddress);
+
+            var currentStep = parseInt($('#current-step').val());
+            var nextStep = currentStep + 1;
+            var url = window.location.href;
+            var newUrl = updateUrlParameter(url, 'step', nextStep);
+            window.location.href = newUrl;
+        });
+
+        function updateUrlParameter(url, param, paramVal) {
+            var newAdditionalURL = "";
+            var tempArray = url.split("?");
+            var baseURL = tempArray[0];
+            var additionalURL = tempArray[1];
+            var temp = "";
+            if (additionalURL) {
+                tempArray = additionalURL.split("&");
+                for (var i = 0; i < tempArray.length; i++) {
+                    if (tempArray[i].split('=')[0] != param) {
+                        newAdditionalURL += temp + tempArray[i];
+                        temp = "&";
+                    }
+                }
+            }
+            var rows_txt = temp + "" + param + "=" + paramVal;
+            return baseURL + "?" + newAdditionalURL + rows_txt;
+        }
     });
 </script>
--- templates/step3-review.php
+++ templates/step3-review.php
@@ -1,48 +1,100 @@
 <div class="wmc-step wmc-step3">
     <h2><?php _e('Schritt 3: Überprüfung', 'woomulticheckout'); ?></h2>
-    <div class="wmc-review-section">
-        <h3><?php _e('Zahlungsmethode', 'woomulticheckout'); ?></h3>
-        <p id="wmc-review-payment-method"><!-- Display selected payment method here --></p>
-    </div>
-    <div class="wmc-review-section">
-        <h3><?php _e('Rabattcode', 'woomulticheckout'); ?></h3>
-        <p id="wmc-review-coupon-code"><!-- Display applied coupon code here --></p>
-    </div>
-    <div class="wmc-review-section">
-        <h3><?php _e('Lieferadresse', 'woomulticheckout'); ?></h3>
-        <p id="wmc-review-shipping-address"><!-- Display shipping address here --></p>
-    </div>
-    <div class="wmc-review-section">
-        <h3><?php _e('Rechnungsadresse', 'woomulticheckout'); ?></h3>
-        <p id="wmc-review-billing-address"><!-- Display billing address here --></p>
-    </div>
-    <div class="wmc-review-section">
-        <h3><?php _e('Bestellung', 'woomulticheckout'); ?></h3>
-        <div id="wmc-review-order"><!-- Display order details here --></div>
-    </div>
-    <button type="button" id="wmc-place-order"><?php _e('Jetzt bestellen', 'woomulticheckout'); ?></button>
+    <form id="wmc-step3-form" method="post">
+        <div class="wmc-review-section">
+            <h3><?php _e('Zahlungsmethode', 'woomulticheckout'); ?></h3>
+            <p id="wmc-review-payment-method">
+                <!-- Display selected payment method here -->
+                <select name="payment_method" id="payment-method">
+                    <?php
+                    $available_gateways = WC()->payment_gateways->get_available_payment_gateways();
+                    foreach ($available_gateways as $gateway) {
+                        echo '<option value="' . esc_attr($gateway->id) . '">' . esc_html($gateway->get_title()) . '</option>';
+                    }
+                    ?>
+                </select>
+            </p>
+        </div>
+        <div class="wmc-review-section">
+            <h3><?php _e('Rabattcode', 'woomulticheckout'); ?></h3>
+            <p id="wmc-review-coupon-code">
+                <!-- Display applied coupon code here -->
+                <input type="text" name="coupon_code" value="<?php echo esc_attr($couponCode); ?>">
+            </p>
+        </div>
+        <div class="wmc-review-section">
+            <h3><?php _e('Lieferadresse', 'woomulticheckout'); ?></h3>
+            <p>
+                <span id="shipping-address-display"><!-- Display shipping address here --></span>
+                <a href="#" id="edit-shipping-address"><?php _e('Bearbeiten', 'woomulticheckout'); ?></a>
+            </p>
+            <textarea name="shipping_address" id="shipping-address" class="hidden"><?php echo esc_textarea($shippingAddress); ?></textarea>
+        </div>
+        <div class="wmc-review-section">
+            <h3><?php _e('Rechnungsadresse', 'woomulticheckout'); ?></h3>
+            <p>
+                <span id="billing-address-display"><!-- Display billing address here --></span>
+                <a href="#" id="edit-billing-address"><?php _e('Bearbeiten', 'woomulticheckout'); ?></a>
+            </p>
+            <textarea name="billing_address" id="billing-address" class="hidden"><?php echo esc_textarea($billingAddress); ?></textarea>
+        </div>
+        <div class="wmc-review-section">
+            <h3><?php _e('Bestellung', 'woomulticheckout'); ?></h3>
+            <div id="wmc-review-order">
+                <!-- Display order details here -->
+                <?php
+                $cart = WC()->cart;
+                if (!empty($cart->get_cart())) {
+                    echo '<ul>';
+                    foreach ($cart->get_cart() as $cart_item_key => $cart_item) {
+                        $_product = apply_filters('woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key);
+                        echo '<li>' . esc_html($_product->get_name()) . ' x ' . esc_html($cart_item['quantity']) . ' - ' . wc_price($_product->get_price() * $cart_item['quantity']) . '</li>';
+                    }
+                    echo '</ul>';
+                }
+                ?>
+            </div>
+        </div>
+        <button type="button" id="wmc-update-order"><?php _e('Änderungen speichern', 'woomulticheckout'); ?></button>
+        <button type="submit" id="wmc-place-order" name="place_order"><?php _e('Jetzt bestellen', 'woomulticheckout'); ?></button>
+    </form>
 </div>
+
 <script>
     document.addEventListener('DOMContentLoaded', function() {
-        // Get the selected payment method from step 2
-        var paymentMethod = localStorage.getItem('paymentMethod');
-        document.getElementById('wmc-review-payment-method').textContent = paymentMethod;
+        // Toggling between display and edit mode for shipping address
+        var editShippingAddressLink = document.getElementById('edit-shipping-address');
+        var shippingAddressDisplay = document.getElementById('shipping-address-display');
+        var shippingAddressTextarea = document.getElementById('shipping-address');
 
-        // Get the applied coupon code from step 2
-        var couponCode = localStorage.getItem('couponCode');
-        document.getElementById('wmc-review-coupon-code').textContent = couponCode;
+        editShippingAddressLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            shippingAddressDisplay.classList.add('hidden');
+            shippingAddressTextarea.classList.remove('hidden');
+        });
 
-        // Get the shipping address from step 1
-        var shippingAddress = localStorage.getItem('shippingAddress');
-        document.getElementById('wmc-review-shipping-address').textContent = shippingAddress;
+        // Toggling between display and edit mode for billing address
+        var editBillingAddressLink = document.getElementById('edit-billing-address');
+        var billingAddressDisplay = document.getElementById('billing-address-display');
+        var billingAddressTextarea = document.getElementById('billing-address');
 
-        // Get the billing address from step 1
-        var billingAddress = localStorage.getItem('billingAddress');
-        document.getElementById('wmc-review-billing-address').textContent = billingAddress;
+        editBillingAddressLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            billingAddressDisplay.classList.add('hidden');
+            billingAddressTextarea.classList.remove('hidden');
+        });
 
-        // Get the order details from the cart
-        // Note: You may need to implement this part depending on how you store and display the order details
-        // var orderDetails = ...;
-        // document.getElementById('wmc-review-order').innerHTML = orderDetails;
+        // Saving updated information
+        var updateOrderButton = document.getElementById('wmc-update-order');
+        updateOrderButton.addEventListener('click', function() {
+            shippingAddressDisplay.textContent = shippingAddressTextarea.value;
+            billingAddressDisplay.textContent = billingAddressTextarea.value;
+
+            // Hide the textareas and display the updated addresses
+            shippingAddressDisplay.classList.remove('hidden');
+            shippingAddressTextarea.classList.add('hidden');
+            billingAddressDisplay.classList.remove('hidden');
+            billingAddressTextarea.classList.add('hidden');
+        });
     });
 </script>
