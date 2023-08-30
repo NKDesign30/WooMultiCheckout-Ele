cabd4973f056c98c34433499540d7d42b9b4860d
Niko.Knez <info@design-nk.de>
2023-08-30 17:47:41 +0200

    woocommerce

--- assets/js/woo-multicheckout.js
+++ assets/js/woo-multicheckout.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
   $('input.qty').on('change', function() {
-      var item_key = $(this).data('key');
+      var item_key = $(this).closest('.cart_item').data('key');
       var quantity = $(this).val();
 
       $.ajax({
@@ -8,14 +8,19 @@ jQuery(document).ready(function($) {
           url: wc_cart_params.ajax_url,
           data: {
               action: 'woocommerce_update_cart',
-              item_key: item_key,
-              quantity: quantity
+              cart: { [item_key]: { qty: quantity } }
           },
           success: function(response) {
               if (response.success) {
                   // Aktualisieren Sie den Gesamtpreis des Warenkorbs
-                  $('.cart_totals .amount').text(response.data.total);
+                  $('.cart_totals .amount').text(response.data.fragments['.cart_totals .amount']);
+              } else {
+                  // Fehlermeldung anzeigen
+                  alert(response.data.messages);
               }
+          },
+          error: function() {
+              alert('Es gab einen Fehler beim Aktualisieren des Warenkorbs.');
           }
       });
   });
