342f2ac8a44834e6fb22c1e4607c6db4822b5616
Niko.Knez <info@design-nk.de>
2023-09-04 15:27:49 +0200

    4

--- templates/step3-review.php
+++ templates/step3-review.php
@@ -60,7 +60,7 @@ defined('ABSPATH') || exit;
             </div>
 
             <!-- Rabattcode -->
-            <div class="wmc-section">
+            <div class="wmc-review-section">
                 <h3>Rabattcode</h3>
                 <?php if (WC()->cart->get_coupon_discount_totals()) : ?>
                     <?php foreach (WC()->cart->get_coupon_discount_totals() as $code => $amount) : ?>
@@ -70,15 +70,28 @@ defined('ABSPATH') || exit;
             </div>
 
             <!-- Lieferadresse -->
-            <div class="wmc-section">
+            <div class="wmc-review-section">
                 <h3>Lieferadresse</h3>
-                <address>
-                    <?php echo ($address = WC()->customer->get_shipping_address()) ? $address : 'Nicht festgelegt'; ?>
-                </address>
+                <?php
+                // Holen Sie sich die Lieferadresse-Details
+                $shipping_first_name = WC()->customer->get_shipping_first_name();
+                $shipping_last_name = WC()->customer->get_shipping_last_name();
+                $shipping_address_1 = WC()->customer->get_shipping_address_1();
+                $shipping_address_2 = WC()->customer->get_shipping_address_2();
+                $shipping_city = WC()->customer->get_shipping_city();
+                $shipping_postcode = WC()->customer->get_shipping_postcode();
+                ?>
+                <p>
+                    <?php echo $shipping_first_name . ' ' . $shipping_last_name; ?><br>
+                    <?php echo $shipping_address_1; ?>
+                    <?php if (!empty($shipping_address_2)) echo '<br>' . $shipping_address_2; ?>
+                    <br>
+                    <?php echo $shipping_postcode . ' ' . $shipping_city; ?>
+                </p>
             </div>
 
             <!-- Rechnungsadresse -->
-            <div class="wmc-section">
+            <div class="wmc-review-section">
                 <h3>Rechnungsadresse</h3>
                 <address>
                     <?php echo ($address = WC()->customer->get_billing_address()) ? $address : 'Nicht festgelegt'; ?>
@@ -100,245 +113,6 @@ defined('ABSPATH') || exit;
     <button id="placeOrder">Bestellen</button>
 </div>
 
-<!-- Styling -->
-<style>
-    #step1,
-    #step2,
-    #step3 {
-        padding: 20px;
-        border: 1px solid #ccc;
-        margin-bottom: 20px;
-    }
-
-    #payment-only .woocommerce-checkout-review-order-table,
-    #payment-only .woocommerce-checkout-review-order {
-        display: none;
-    }
-
-    .wmc-review-section {
-        border: 1px solid #f5f5f5;
-        border-radius: 20px;
-        padding: 20px;
-        margin-bottom: 20px;
-    }
-
-    .wmc-review-section h3 {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        font-size: 18px;
-        font-weight: bold;
-        margin-bottom: 10px;
-    }
-
-    .wmc-review-section p {
-        font-size: 16px;
-    }
-
-    .edit-link {
-        cursor: pointer;
-        color: blue;
-        text-decoration: underline;
-    }
-
-    .hidden {
-        display: none;
-    }
-
-    .wmc-label {
-        color: #c3c3c3;
-    }
-
-    .wmc-cart-item-title,
-    .wmc-cart-item-price {
-        font-weight: bold;
-    }
-
-    .wmc-value {
-        font-weight: bold;
-    }
-
-    .custom-cart-totals-box {
-        width: 100%;
-    }
-
-    .wmc-cart-item-image img {
-        width: 50px;
-        height: auto;
-    }
-
-    .wmc-cart-item {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 10px;
-    }
-
-    .wmc-cart-item-details {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        width: 100%;
-    }
-
-    .wmc-cart-item-title {
-        flex: 1;
-    }
-
-    .wmc-cart-item-price {
-        flex: 0;
-        margin: 15px;
-    }
-
-    .wmc-cart-totals {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        flex-wrap: wrap;
-    }
-
-    .wmc-cart-totals .cart-row {
-        width: 100%;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 10px;
-    }
-
-    .wmc-cart-totals .cart-row span:first-child {
-        font-weight: bold;
-    }
-
-    .wmc-cart-totals .cart-row:last-child span:first-child,
-    .wmc-cart-totals .cart-row:last-child span:last-child {
-        font-weight: bold;
-    }
-
-    .wmc-cart-totals .cart-row.rabatt span:last-child {
-        color: red;
-    }
-
-    .progress-bar-text {
-        margin-top: 15px;
-        width: 100%;
-        font-size: 12px;
-    }
-
-    @media (min-width: 768px) {
-        .wmc-review-container {
-            display: flex;
-            justify-content: space-between;
-        }
-
-        .wmc-review-left,
-        .wmc-review-right {
-            width: 48%;
-        }
-
-        .wmc-cart-item-image img {
-            width: 50px;
-            /* Reduzierte Bildgröße */
-        }
-
-        .address-line {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-
-        .wmc-label {
-            flex: 0;
-        }
-
-        .wmc-value {
-            flex: 1;
-            text-align: right;
-        }
-
-        .wmc-cart-item {
-            display: flex;
-            align-items: center;
-        }
-
-        .wmc-cart-item-image {
-            flex: 0.5;
-            /* Reduzierte Flex-Basis für Bilder */
-            margin: 0 10px;
-        }
-
-        .wmc-cart-item-title {
-            flex: 2;
-            /* Erhöhte Flex-Basis für den Titel */
-            overflow: hidden;
-            text-overflow: ellipsis;
-            white-space: nowrap;
-            margin: 0 10px;
-        }
-
-        .wmc-cart-item-dropdown {
-            flex: 0.5;
-            /* Reduzierte Flex-Basis für Dropdown */
-            margin: 0 10px;
-        }
-
-        .wmc-cart-item-price,
-        .wmc-cart-item-button {
-            flex: 1;
-            margin: 0 10px;
-        }
-
-        .wmc-cart-item-quantity {
-            width: 80px;
-            /* Reduzierte Dropdown-Breite */
-            padding: 5px;
-            border-radius: 5px;
-            border: 1px solid #ccc;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            font-size: 14px;
-            color: #333;
-            background-color: #fff;
-            text-align: center;
-        }
-
-        .wmc-cart-item-quantity:hover {
-            border-color: #999;
-            background-color: #f7f7f7;
-        }
-
-        .wmc-cart-item-quantity:focus {
-            outline: none;
-            box-shadow: none;
-        }
-
-        .wmc-cart-item-remove {
-            background-color: transparent;
-            /* Entfernt den Hintergrund */
-            color: #f44336;
-            /* Setzt die Farbe des "X" auf Rot */
-            padding: 5px 8px;
-            /* Passt die Polsterung an, um den Button kleiner zu machen */
-            border: none;
-            border-radius: 50%;
-            /* Macht den Button kreisförmig */
-            font-weight: bold;
-            /* Macht das "X" fett */
-            font-size: 18px;
-            /* Erhöht die Schriftgröße des "X" */
-            cursor: pointer;
-            transition: background-color 0.3s ease, color 0.3s ease;
-            display: flex;
-            justify-content: flex-end;
-            /* Fügt eine Übergangsfarbe hinzu */
-        }
-
-        .wmc-cart-item-remove:hover {
-            background-color: #f44336;
-            /* Setzt den Hintergrund beim Überfahren auf Rot */
-            color: #fff;
-            /* Setzt die Farbe des "X" beim Überfahren auf Weiß */
-        }
-    }
-</style>
 
 <!-- JavaScript for navigation -->
 <script>
@@ -370,6 +144,43 @@ defined('ABSPATH') || exit;
                 // Wählen Sie die entsprechende Zahlungsmethode in Schritt 3 aus
                 $('input[name="payment_method"][value="' + selectedPaymentMethod + '"]').prop('checked', true);
             }
+        });
+    });
+    jQuery(document).ready(function($) {
+        $('#goToStep2').click(function(e) {
+            e.preventDefault(); // Verhindert das normale Absenden des Formulars
+
+            var billingData = {
+                'billing_first_name': $('input[name="billing_first_name"]').val(),
+                'billing_last_name': $('input[name="billing_last_name"]').val(),
+                'billing_company': $('input[name="billing_company"]').val(),
+                'billing_address_1': $('input[name="billing_address_1"]').val(),
+                'billing_address_2': $('input[name="billing_address_2"]').val(),
+                'billing_city': $('input[name="billing_city"]').val(),
+                'billing_postcode': $('input[name="billing_postcode"]').val(),
+                'billing_country': $('select[name="billing_country"]').val(),
+                'billing_state': $('select[name="billing_state"]').val(),
+                'billing_phone': $('input[name="billing_phone"]').val(),
+                'billing_email': $('input[name="billing_email"]').val()
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: wc_checkout_params.ajax_url,
+                data: {
+                    'action': 'save_address_data',
+                    'billing_data': billingData
+                },
+                success: function(response) {
+                    if (response.success) {
+                        // Hier können Sie Code hinzufügen, um zum nächsten Schritt zu wechseln
+                        $('#step1').hide();
+                        $('#step2').show();
+                    } else {
+                        alert('Es gab ein Problem beim Speichern Ihrer Daten. Bitte versuchen Sie es erneut.');
+                    }
+                }
+            });
         });
     });
 </script>
